ccaf741b20a6d88b5e16dc69be3c53a8ec7a18ca
chenyuxi <972389149@qq.com>
2022-10-10 17:46:43 +0800

    fix: 修复tiny依赖问题

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,7 +1,9 @@
 import execa from 'execa'
 import fs from 'fs'
 import { resolve } from 'path'
-export const vPkgDir = fs.readdirSync(resolve('.', 'packages'))
+export const vPkgDir = fs
+  .readdirSync(resolve('.', 'packages'))
+  .filter(path => !path.includes('.DS_Store'))
 export const run = (bin: string, args: string[], opts: any = {}): void => {
   execa(bin, args, { stdio: 'inherit', ...opts })
 }
